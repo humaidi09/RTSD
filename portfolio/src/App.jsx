9ee603aa0a4e82e74d64934dcb87032cd3e3745f
humaidi09 <hussainahmed7p@gmail.com>
2026-08-25 16:46:52 +0100

    Fix Tailwind CSS

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -12,6 +12,7 @@ export default function App() {
   return (
     <div className="relative isolate min-h-screen overflow-x-hidden bg-void text-ink">
       <AmbientBackground />
+
       <Navbar />
 
       <main>
@@ -35,9 +36,9 @@ function AmbientBackground() {
       aria-hidden="true"
       className="pointer-events-none fixed inset-0 -z-10 overflow-hidden"
     >
-      <div className="absolute -top-32 left-1/2 h-[38rem] w-[38rem] -translate-x-1/2 rounded-full bg-neonCyan/[0.06] blur-[120px]" />
+      <div className="absolute -top-32 left-1/2 h-[38rem] w-[38rem] -translate-x-1/2 -translate-y-1/2 rounded-full bg-neonCyan/[0.08] blur-[120px]" />
 
-      <div className="absolute -right-[10%] bottom-0 h-[32rem] w-[32rem] rounded-full bg-neonPurple/[0.07] blur-[130px]" />
+      <div className="absolute -right-[10%] bottom-0 h-[32rem] w-[32rem] rounded-full bg-neonPurple/[0.10] blur-[130px]" />
 
       <div className="absolute inset-0 bg-dot-grid opacity-[0.25]" />
     </div>
