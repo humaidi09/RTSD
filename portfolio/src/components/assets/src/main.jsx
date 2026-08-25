d22373e827099d255b5ed195730e3b97404a58c8
humaidi09 <hussainahmed7p@gmail.com>
2026-08-25 16:31:59 +0100

    Fix portfolio App

--- portfolio/src/components/assets/src/main.jsx
+++ portfolio/src/components/assets/src/main.jsx
@@ -2,8 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { ThemeProvider } from './context/ThemeContext.jsx'
-import { ToastProvider } from './context/ToastContext.jsx'
+import { ThemeProvider } from './context/ThemeContext.js'
+import { ToastProvider } from './context/ToastContext.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
