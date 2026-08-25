3a17e1293ab9a678b0451b36afb9e09e7b09ac49
humaidi09 <hussainahmed7p@gmail.com>
2026-08-25 16:55:56 +0100

    Fix Tailwind CSS

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -36,7 +36,7 @@ function AmbientBackground() {
       aria-hidden="true"
       className="pointer-events-none fixed inset-0 -z-10 overflow-hidden"
     >
-      <div className="absolute -top-32 left-1/2 h-[38rem] w-[38rem] -translate-x-1/2 -translate-y-1/2 rounded-full bg-neonCyan/[0.08] blur-[120px]" />
+      <div className="absolute -top-32 left-1/2 h-[38rem] w-[38rem] -translate-x-1/2 rounded-full bg-neonCyan/[0.08] blur-[120px]" />
 
       <div className="absolute -right-[10%] bottom-0 h-[32rem] w-[32rem] rounded-full bg-neonPurple/[0.10] blur-[130px]" />
 
